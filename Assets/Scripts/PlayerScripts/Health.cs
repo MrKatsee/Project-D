f8f97705873c0334327853d789a97d8a3632dd96
KimHoseong <ghtjd000129@naver.com>
2018-05-30 23:00:41 +0900

    스킬바 구현 및 스킬 발동 구현

--- Assets/Scripts/PlayerScripts/Health.cs
+++ Assets/Scripts/PlayerScripts/Health.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI; //UI선언
 
 public class Health : MonoBehaviour {
 
+    public int isPlayer;
+    public float guageStack = 0f;
     public int HP = 3;
     public GameObject[] P1Hp = new GameObject[3];
     public GameObject[] P2Hp = new GameObject[3];
+    public float skillBar = 0;
     int HP_temp;
 
     // Use this for initialization
@@ -28,12 +31,131 @@ public class Health : MonoBehaviour {
             HP_temp = HP;
         }
 
-	}
+        if (skillBar <= 1f)
+        {
+            skillBar += 0.2f * Time.deltaTime;
+        }
+
+        if (isPlayer == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                if (guageStack == 0f)
+                {
+                    if (skillBar >= 0.25f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+                else if (guageStack == 0.25f)
+                {
+                    if (skillBar >= 0.5f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+                else if (guageStack == 0.5f)
+                {
+                    if (skillBar >= 0.75f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+                else if (guageStack == 0.75f)
+                {
+                    if (skillBar >= 1f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                if (guageStack == 0.25f)
+                {
+                    skillBar -= 0.25f;
+                    guageStack = 0;
+                }
+                else if (guageStack == 0.5f)
+                {
+                    skillBar -= 0.50f;
+                    guageStack = 0;
+                }
+                else if (guageStack == 0.75f)
+                {
+                    skillBar -= 0.75f;
+                    guageStack = 0;
+                }
+                else if (guageStack == 1f)
+                {
+                    skillBar -= 1f;
+                    guageStack = 0;
+                }
+            }
+        }
+
+        if (isPlayer == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                if (guageStack == 0f)
+                {
+                    if (skillBar >= 0.25f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+                else if (guageStack == 0.25f)
+                {
+                    if (skillBar >= 0.5f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+                else if (guageStack == 0.5f)
+                {
+                    if (skillBar >= 0.75f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+                else if (guageStack == 0.75f)
+                {
+                    if (skillBar >= 1f)
+                    {
+                        guageStack += 0.25f;
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                if (guageStack == 0.25f)
+                {
+                    skillBar -= 0.25f;
+                    guageStack = 0;
+                }
+                else if (guageStack == 0.5f)
+                {
+                    skillBar -= 0.50f;
+                    guageStack = 0;
+                }
+                else if (guageStack == 0.75f)
+                {
+                    skillBar -= 0.75f;
+                    guageStack = 0;
+                }
+                else if (guageStack == 1f)
+                {
+                    skillBar -= 1f;
+                    guageStack = 0;
+                }
+            }
+        }
+    }
 
     public void HealthDecrease()
     {
         HP -= 1;
-        Debug.Log(HP);
     }
 
     void AltHP()
---
+++ Assets/Scripts/UI/SkillBarIncrease.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillBarIncrease : MonoBehaviour {
+
+    public Image skillBarGuage;
+    public int isBar;
+    GameObject temp;
+    public int isPlayer;
+    Health playerHealth;
+
+    // Use this for initialization
+    void Start() {
+        skillBarGuage = GetComponent<Image>();
+
+        if (isPlayer == 1) {
+            playerHealth = GameObject.Find("Player1").GetComponent<Health>();
+        }
+
+        if (isPlayer == 2)
+        {
+            playerHealth = GameObject.Find("Player2").GetComponent<Health>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBar == 1)
+        {
+            skillBarGuage.fillAmount = playerHealth.skillBar;
+        }
+        if (isBar == 0)
+        {
+            skillBarGuage.fillAmount = playerHealth.guageStack;
+        }
+    }
+}
